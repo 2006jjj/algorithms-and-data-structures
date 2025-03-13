5d1c8bea97b13aa252b27de1797c1cdc87d31812
死 yohji <47281780+2006jjj@users.noreply.github.com>
2025-03-13 15:03:00 +0300

    Create 2.9 Количество равных максимальному.cpp

---
+++ 2. Линейный поиск/2.9 Количество равных максимальному.cpp
@@ -0,0 +1,57 @@
+/*Задача 2.9
+Последовательность состоит из натуральных чисел и завершается числом 0. 
+Всего вводится не более 10000 чисел (не считая завершающего числа 0). 
+Определите, сколько элементов этой последовательности равны ее наибольшему элементу.
+Числа, следующие за числом 0, считывать не нужно.
+
+Формат ввода
+Вводится последовательность целых чисел, оканчивающаяся числом 0 (само число 0 в последовательность не входит).
+
+Формат вывода
+Выведите ответ на задачу.
+
+Пример 1
+Ввод:
+1 7 9 0
+Вывод:
+1
+
+Пример 2
+Ввод:
+1 3 3 1 0
+Вывод:
+2*/
+
+#include <iostream> 
+using namespace std;
+
+int main()
+{setlocale(LC_ALL, ".1251"); system("color 0F"); srand(time(NULL));
+
+const int MAX = 10000;
+int array[MAX]{}, i, count = 0;
+
+for (int j = 0; j < MAX; j++)
+{
+	cin >> i;
+	array[j] = i;
+	if (array[j] == 0)
+		break;
+}
+int maxnumber=0, finalcount = 0;
+for (int j = 0; array[j] !=0 ; j++)
+{
+	if (array[j] > maxnumber)
+		maxnumber = array[j];
+	cout << array[j] << " ";
+}
+cout << "\nmaxnumber="<<maxnumber << endl;
+for (int j = 0; array[j] != 0; j++)
+{
+	if (array[j] == maxnumber)
+		finalcount++;
+}
+cout << "\nОтвет:" << finalcount << endl;
+
+cout << endl; system("pause"); return 0;
+}

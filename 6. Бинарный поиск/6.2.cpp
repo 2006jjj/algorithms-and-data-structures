549cefeb31c1d224f70acab4e32d74e8a4de7fda
jaki <47281780+2006jjj@users.noreply.github.com>
2025-06-18 17:46:54 +0300

    Update 6.2.cpp

--- 6. Бинарный поиск/6.2.cpp
+++ 6. Бинарный поиск/6.2.cpp
@@ -2,3 +2,43 @@
 Юра решил подготовиться к собеседованию в Яндекс. Он выбрал на сайте leetcode N задач.
 В первый день Юра решил K задач, а в каждый следующий день Юра решал на одну задачу больше, чем в предыдущий день.
 Определите, сколько дней уйдет у Юры на подготовку к собеседованию.*/
+
+
+#include <iostream>
+using namespace std;
+
+int lbinsearch(int, int, int, int);
+bool checkendowment(int, int, int);
+
+int main()
+{setlocale(LC_ALL, ".1251"); system("color 0F"); srand(time(NULL)); ios::sync_with_stdio(false); cin.tie(nullptr);
+
+int N, K;
+cout << "Введите общее количество задач для достижения цели:";
+cin >> N;
+cout << "Введите количество задач которые Юра решил за один день:";
+cin >> K;
+
+int result = lbinsearch(0, N, N, K);
+cout << "\nRESULT=" << result<<" days";
+
+cout << endl;  system("pause"); return 0;
+}
+
+int lbinsearch(int left, int right, int n, int k)
+{
+	while (left < right)
+	{
+		int mid = (left + right) / 2;
+		if (checkendowment(mid, n, k))
+			right = mid;
+		else
+			left = mid + 1;
+	}
+	return left;
+}
+
+bool checkendowment(int days, int n, int k) 
+{
+	return (k+(k+days-1)) * days / 2 >= n;
+}
